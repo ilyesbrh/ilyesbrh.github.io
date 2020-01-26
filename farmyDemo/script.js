88ee35bbe150c0f1d793626213b7b1081b2bfd97
desktop <brhilyes@gmail.com>
2020-01-26 19:04:07 +0100

    part one done

--- farmyDemo/script.js
+++ farmyDemo/script.js
@@ -1,3 +1,13 @@
+var image;
+var Token;
+var response;
+
+function init() {
+    let url = new URL(location.href);
+    let searchParams = new URLSearchParams(url.search);
+    Token = searchParams.get('token');
+}
+
 function flipIt(flip) {
     console.log('clicked');
 
@@ -24,4 +34,55 @@ function fillData() {
     document.getElementById('4').style.width = 10 + '%';
 }
 
-fillData();
+function openUpload() {
+    document.getElementById('upload-image').click();
+};
+
+function changeToken(token) {
+    token = event.target.value;
+}
+
+function changeImage(event) {
+    var output = document.getElementById('display-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    image = event.target.files[0];
+}
+
+function setToken() {
+
+    Token = document.getElementById('token-input').value;
+}
+
+function upload() {
+
+
+    if (!image) {
+        $("#imageModel").modal();
+    }
+
+    if (Token && Token.length !== 0) {
+
+        const file = new Blob([image], { type: 'image/png' });
+        const formData = new FormData();
+        formData.append('image', file, file.filename + '.png');
+        formData.append('token', Token);
+
+        const url = 'http://farmy-backend.herokuapp.com/api/v1/test-model';
+
+        axios.post(url, formData)
+            .then(function(res) {
+                console.log(res);
+                response = res;
+                flipIt(true);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+
+    } else {
+        $("#TokenModel").modal();
+    }
+}
+
+
+init();
