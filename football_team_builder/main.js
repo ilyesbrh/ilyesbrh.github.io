b072431888aaa5fc68bd7f938c8ea5186d433413
desktop <brhilyes@gmail.com>
2021-12-21 23:31:52 +0100

    team builder

---
+++ football_team_builder/main.js
@@ -0,0 +1,38 @@
+const player = document.querySelectorAll('.player')[0];
+
+const players = [
+    { name: 'Z.Amine', number: 9, x: '55%', y: '50%', team: false },
+    { name: 'M.Nazim ', number: 7, x: '35%', y: '50%', team: false },
+    { name: 'B.ilies', number: 10, x: '46%', y: '65%', team: false },
+    { name: 'M.khairdine', number: 4, x: '80%', y: '70%', team: false },
+    { name: 'B.Iqbal', number: 3, x: '45%', y: '80%', team: false },
+    { name: 'M.Yassine', number: 5, x: '10%', y: '70%', team: false },
+
+    { name: 'Z.Nadir', number: 5, x: '10%', y: '15%', team: true },
+    { name: 'A.Rafik', number: 4, x: '80%', y: '15%', team: true },
+    { name: 'A.Charef', number: 3, x: '45%', y: '10%', team: true },
+    { name: 'Invité 1', number: 8, x: '10%', y: '35%', team: true },
+    { name: 'D.Walid', number: 9, x: '80%', y: '35%', team: true },
+    { name: 'Invité 2', number: 10, x: '45%', y: '30%', team: true },
+]
+
+players.map(v => {
+
+    const p = player.cloneNode(true);
+
+    document.getElementById('container').appendChild(p);
+
+    const text = p.querySelector('.player-text');
+    const number = p.querySelector('.pos-text');
+
+    p.classList.remove('d-none');
+    if (v.team) p.classList.add('red');
+
+    p.style.left = v.x;
+    p.style.top = v.y;
+
+    text.innerHTML = v.name;
+    number.innerHTML = v.number;
+
+
+})
